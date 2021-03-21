377d5bb8d1f1d7573fcce01a25a75c88daecb5f3
Andrew Chau <achau6042@gmail.com>
2021-03-21 15:54:23 -0400

    Add routes to other screens

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
+import { Switch, Route, Redirect } from 'react-router';
 
 import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
-
       <Navbar />
+      <div className="Screen">
+        <Switch>
+          <Route path="/MHW">
+            MHW Screen
+          </Route>
+          <Route path="/MHWI">
+            MHWI Screen
+          </Route>
+          <Route path="/Rise">
+            Rise Screen
+          </Route>
+          <Route path="/">
+            <Redirect to="/MHW" />
+          </Route>
+        </Switch>
+      </div>
     </div>
   );
 }
